9db818968846e333a230f6872c2db6b1f090dd67
SimonBigverdi <simon95big@gmail.com>
2024-06-04 22:14:59 -0700

    Update Disable_Stale_Accounts.ps1

--- Disable_Stale_Accounts.ps1
+++ Disable_Stale_Accounts.ps1
@@ -1,3 +1,5 @@
+# Script will find and disable enabled accounts that have not logged in the past 90 days using the LastLogonTimestamp attribute
+
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
