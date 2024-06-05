9303b2f60d08110de0d11151b2e55065b88beddc
SimonBigverdi <simon95big@gmail.com>
2024-06-04 22:20:05 -0700

    Update Install_ADDS.ps1

--- Install_ADDS.ps1
+++ Install_ADDS.ps1
@@ -1,4 +1,4 @@
-# Configurations below are made up and can be modified as needed
+# This script will assign a static IP to a server, will install the ADDS role, and will promote the server to a Domain Controller
 
 # Assign a static IP address to the server
 $IPAddress = "192.168.50.50"
@@ -9,7 +9,7 @@ $InterfaceIndex = (Get-NetAdapter).InterfaceIndex
 New-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress $IPAddress -PrefixLength 24 -DefaultGateway $Gateway
 Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses $DNS
 
-# Install the AD DS role and promote the server to a domain controller
+# Install the ADDS role and promote the server to a Domain Controller
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName "Domain.com" `
                   -DomainNetbiosName "Domain" `
